33b8d267d841dec168cf4d10c2982b4b6069ce57
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2021-12-21 20:38:11 -0300

    add category controller and views

--- src/Quiz.App/Controllers/CategoryController.cs
+++ src/Quiz.App/Controllers/CategoryController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Quiz.App.Infrastructure.Repositories;
+using Quiz.App.Models;
 
 namespace Quiz.App.Controllers
 {
     public class CategoryController : Controller
     {
-        // GET
-        public IActionResult Index()
+        private readonly IRepository<Category> _categoryRepository;
+
+        public CategoryController(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _categoryRepository.GetDataAsync());
+        }
+
+        public async Task<IActionResult> Create()
         {
             return View();
         }
